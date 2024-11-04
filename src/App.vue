2418ed32b4fc7fc8401b3690852dce5d18328b1a
Rubel Mahmud <vxlrubel@gmail.com>
2024-11-04 23:56:02 +0600

    update app file.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import HeaderView from './components/HeaderView.vue';
 </script>
-
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
-
-  <RouterView />
+  <HeaderView/>
 </template>
-
-<style scoped></style>
