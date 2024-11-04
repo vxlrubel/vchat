cf227ae14b2f8b20fa6bdbba214db665faf9a87f
Rubel Mahmud <vxlrubel@gmail.com>
2024-11-04 23:56:55 +0600

    create header file.

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeIcon from './icons/HomeIcon.vue'
+import ProfileIcon from './icons/ProfileIcon.vue'
+</script>
+
+<template>
+    <header class="clearfix">
+        <div class="container">
+            <RouterLink to="/" class="d-inline-flex align-items-center gap-1 text-decoration-none me-3">
+                <HomeIcon /> Home
+            </RouterLink>
+            <RouterLink to="/about" class="d-inline-flex align-items-center gap-1 text-decoration-none">
+                <ProfileIcon /> About
+            </RouterLink>
+    <br>
+            
+        </div>
+    </header>
+    <RouterView />
+</template>
